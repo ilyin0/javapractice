5fde7b32a57240abb1f384fb807aea6b39f4ee5c
ilyin0 <ilyindeveloper@gmail.com>
2020-05-07 16:52:27 +0300

    Change project structure for supporting maven

--- YouRecipe/src/main/java/by/bsu/ilyin/Main.java
+++ YouRecipe/src/main/java/by/bsu/ilyin/Main.java
@@ -2,6 +2,8 @@ package by.bsu.ilyin;
 
 import by.bsu.ilyin.UI.ConsoleUI;
 import by.bsu.ilyin.UI.UI;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Main {
     public static void main(String[] args) throws Exception {
